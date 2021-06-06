fcc16763b7f6895ae13856b9c6e5e4ed2f84961d
Luis-Gomez98 <gom18291@uvg.edu.gt>
2021-06-05 19:35:53 -0600

    Ultima parte dipolos
    Corrección en Dipolos_2 y  código en Dipolos_4 para el último inciso.

--- Dipolos_4.m
+++ Dipolos_4.m
@@ -3,44 +3,48 @@
 %Diego Aguilar;18044
 %Helder Ovalle;18349
 %Luis Gomez;18291
-%Dipolos2
+%Dipolos4
 %%
 
 %Realizar las graficas en coordenadas esfericas de los campos y de los potenciales encontrados en 
 %el inciso anterior.
 %clearvars
+%po = 10;
+
 e0 = 8.85 .*10 .^-12;
-po = 0.000001;
+const1 = 4.*pi.*e0;
 
-R = linspace(-5,5);
-theta = linspace(0,pi/4,100)';
-alfa = linspace(0,2*pi,100)';
+R = linspace(-1,1);
+phi = linspace(0,pi/2,100)';
+theta = linspace(0,2*pi,100)';
 
-[theta,alfa] = meshgrid(theta,alfa);
+[phi,theta] = meshgrid(phi,theta);
 
 R = meshgrid(R);
 
 
-[X,Y,Z] = sph2cart(alfa,theta,R);
+[X,Y,Z] = sph2cart(theta,phi,R);
 
-V = @(R,alfa,theta) (((po .* cos(theta-alfa))./(4 .* pi .* e0 * R.^2))) 
+V = @(R,theta) ((1/const1).*(((1/4)-(R.^2)/120).*((((3.*((cos(theta)).^2))-1)/2))));
 
 figure(1);
-surf(X,Y,V(R,alfa,theta));
+surf(X,Y,V(R,theta));
 xlabel('X')
 ylabel('Y')
-zlabel('Potencial Electrico PX')
+zlabel('Potencial Electrico')
 view (-47,32)
 colorbar
+
+
 %Obtener el campo eléctrico mediante el gradiente del potencial
-[Exa, Eya] = gradient(V(R,alfa,theta));
+[Exa, Eya] = gradient(V(R,theta));
 Exa = -Exa;
 Eya = Eya./R;
 Eza = sqrt(Exa.^2+Eya.^2);
 
+
 figure(2);
 quiver3(X,Y,X,Exa,Eya,Eza)
-grid on
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
